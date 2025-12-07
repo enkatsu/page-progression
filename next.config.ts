2f5179821695d5813b1b7ef005ff493c1d86c3f8
Katsuya Endoh <studio@enkatsu.org>
2025-12-07 22:40:35 +0900

    update

--- next.config.ts
+++ next.config.ts
@@ -1,5 +1,6 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
   output: 'export',
   webpack: (config, { isServer }) => {
     console.log('🔧 Webpack config - isServer:', isServer);
@@ -32,4 +33,4 @@ const nextConfig = {
   turbopack: {},
 };
 
-module.exports = nextConfig;
+export default nextConfig;
